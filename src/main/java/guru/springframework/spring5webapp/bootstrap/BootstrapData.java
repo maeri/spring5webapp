00518e9ac99e06c154ecec626cc65c1dc579cabb
zawadma <marcin.zawadzki@ext.ec.europa.eu>
2020-10-18 07:36:59 +0200

    added BootstrapData

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -3,57 +3,67 @@ package guru.springframework.spring5webapp.bootstrap;
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.domain.Publisher;
-import guru.springframework.spring5webapp.repository.AuthorRepository;
-import guru.springframework.spring5webapp.repository.BookRepository;
-import guru.springframework.spring5webapp.repository.PublisherRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * Created by jt on 12/23/19.
+ */
 @Component
-public class BootstrapData implements CommandLineRunner {
+public class BootStrapData implements CommandLineRunner {
 
-	private Logger logger = LoggerFactory.getLogger(BootstrapData.class);
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-	private AuthorRepository authorRepository;
-	private BookRepository bookRepository;
-	private PublisherRepository publisherRepository;
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
 
-	public BootstrapData(AuthorRepository authorRepository,
-						 BookRepository bookRepository,
-						 PublisherRepository publisherRepository) {
-		this.authorRepository = authorRepository;
-		this.bookRepository = bookRepository;
-		this.publisherRepository = publisherRepository;
-	}
+    @Override
+    public void run(String... args) throws Exception {
 
-	public void run(String... args) throws Exception {
-		Book book = new Book("first Book", "123");
-		Author author = new Author("firstName", "lastName");
-		Publisher publisher = new Publisher("publisher", "address", "city", "state", "zip");
+        System.out.println("Started in Bootstrap");
 
-		author.getBooks().add(book);
-		book.getAuthors().add(author);
-		publisherRepository.save(publisher);
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
 
-		book.setPublisher(publisher);
-		publisher.getBooks().add(book);
+        publisherRepository.save(publisher);
 
-		authorRepository.save(author);
-		bookRepository.save(book);
-		publisherRepository.save(publisher);
+        System.out.println("Publisher Count: " + publisherRepository.count());
 
-		logger.info("Count number of records (author): " + authorRepository.count());
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
 
-		logger.info("Count number of records (publisher): " + publisherRepository.count());
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
 
-		for (Publisher pub : publisherRepository.findAll()) {
-//			for (Book bk : pub.getBooks()) {
-				logger.info("Books that was published by publisher " + pub);
-//			}
-		}
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
 
-	}
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
+    }
 }
